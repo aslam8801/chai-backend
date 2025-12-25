7b7919db0ed015112eb26276fa2bafb0d30c75cf
Aslam Kureshi <kureshiaslam176@gmail.com>
2025-12-25 17:38:52 +0530

    .

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,7 +57,11 @@ userSchema.pre('save', async function(next){
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password)    
+    // Check if both exist and are strings before comparing
+    if (!password || !this.password) {
+        return false; 
+    }
+    return await bcrypt.compare(String(password), this.password)    
 }
 
 userSchema.methods.generateAccessToken = function(){
